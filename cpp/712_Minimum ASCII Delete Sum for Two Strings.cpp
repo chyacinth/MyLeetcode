55299f71cad89dfc860fb36c9d608edc6e8a3b2b
chyacinth <chyacinthz@gmail.com>
2018-08-28 19:52:42 -0500

    712

---
+++ cpp/712_Minimum ASCII Delete Sum for Two Strings.cpp
@@ -0,0 +1,27 @@
+class Solution {
+ public:
+  int minimumDeleteSum(string s1, string s2) {
+    int l1 = s1.length();    
+    int l2 = s2.length();
+    vector< vector<int> > dp(l1 + 1, vector<int>(l2 + 1));
+    dp[0][0] = 0;
+    for (int i = 0; i <= l1; ++i) {
+      for (int j = 0; j <= l2; ++j) {        
+        if (i == 0 && j == 0) {
+          dp[i][j] = 0;
+        } else if (i == 0) {
+          dp[i][j] = dp[i][j - 1] + s2[j - 1];
+        } else if (j == 0) {
+          dp[i][j] = dp[i - 1][j] + s1[i - 1];
+        } else {
+          dp[i][j] = min({dp[i - 1][j] + s1[i - 1],
+                          dp[i][j - 1] + s2[j - 1],
+                          dp[i - 1][j - 1] + 
+                            (s1[i - 1] == s2[j - 1]?0:s1[i - 1] + s2[j - 1])
+                          });
+        }
+      }
+    }
+    return dp[l1][l2];
+  }
+};
